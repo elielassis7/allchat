57d60a3fe7ac552a9ae66da8aced7ff742381cb2
Eliel Assis <eliel.assis7@hotmail.com>
2025-08-14 15:32:00 -0300

    add 2 new pages

---
+++ src/pages/Consultancy/index.tsx
@@ -0,0 +1,9 @@
+export function Consultancy() {
+  return (
+    <h1>
+      Consultoria
+    </h1>
+  )
+}
+
+//https://crm.allchat.com.br/proposal/34/3ad794782b5226374f043dc357b2b6a5
---
+++ src/pages/Monitoring/index.tsx
@@ -0,0 +1,11 @@
+
+
+export function Monitoring() {
+  return (
+    <h1>
+      Monitoramento
+    </h1>
+  )
+}
+
+//https://crm.allchat.com.br/proposal/25/eb85c28a510801a8153e67a069f32815
